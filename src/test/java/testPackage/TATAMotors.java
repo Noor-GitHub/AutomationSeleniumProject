7354d7ed71f5f51a371aca12be600c0ceb06f35f
Noor-GitHub <noor2movie@gmail.com>
2023-09-26 15:24:29 +0530

    TATA Motors Class name has been changed - Team Lead

--- src/test/java/testPackage/TATAMotors.java
+++ src/test/java/testPackage/TATAMotors.java
@@ -3,7 +3,7 @@ package testPackage;
 import org.testng.Reporter;
 import genericUtilities.BaseClass;
 
-public class TATAMototrs extends BaseClass{
+public class TATAMotors extends BaseClass{
 
 	public static void main(String[] args) {
 		Reporter.log("TATA Motors" , true);
